2ee855b5caf782cb8a2cafa03b980262389a5acb
preeti271993 <preetipandey1@hotmail.com>
2020-01-30 11:41:08 +0530

    4 files modified, 3 added, 1 deleted

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-product",
+  templateUrl: "./product/product.component.html",
+  styleUrls: ["./product/product.component.css"]
+})
+
+export class ProductComponent{
+   
+}
